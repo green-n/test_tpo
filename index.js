e7098cd089e5491b18374305e90876f5ff95a5dc
green-n <65806207+green-n@users.noreply.github.com>
2022-09-24 11:59:38 +0000

    thisrd lab done check awaits

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 
 const Calculate =(action,firstNum,secondNum)=>{
+    
     if(action<0 || action > 4) return "incorrect input on action"
     else if(secondNum == 0)return "incorrect input on second number"
     else if(action == 1)return (firstNum + secondNum).toFixed(3)
@@ -11,14 +12,20 @@ const Calculate =(action,firstNum,secondNum)=>{
 
 }
 
-const MiddleCalculations = (firstNum,secondNum) =>{
-    if(firstNum<0)return "incorrect input"
-    if(secondNum<0)return "incorrect input"
+const MiddleCalculations = (firstNum,secondNum,errorNum) =>{
+    if(errorNum != undefined)return "exesive input"
+    if(firstNum == undefined || secondNum==undefined)return "insufisiant input"
+
+    if(typeof(firstNum) != "number" || typeof(secondNum) != "number")return "input is not a numbner"
+
+
+    if(firstNum<0 || secondNum<0)return "incorrect input"
     average = (firstNum+secondNum)/2;
     geometricMean = Math.sqrt(firstNum*secondNum)
-    return {average:average,geometricMean:geometricMean}
+    return {average:Number(parseFloat(average).toFixed(5)),geometricMean:Number(parseFloat(geometricMean.toFixed(5)))}
 }
 
+console.log(MiddleCalculations(1,2,3))
 
 
 
--- test.js
+++ test.js
@@ -10,30 +10,6 @@ const {Calculate} = require('./index')
 const {MiddleCalculations} = require('./index')
 
 
-describe('', () => {
-    it('should return 2 и 2', () => {
-      const actualResult = MiddleCalculations(2,2)
-  
-      const expectedResult = {
-        average:2,
-        geometricMean:2
-    };
-  
-      expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
-    });
-  });
-describe('', () => {
-    it('should return 5 и 2', () => {
-      const actualResult = MiddleCalculations(2,2)
-  
-      const expectedResult = {
-        average:3.5,
-        geometricMean:2
-    };
-  
-      expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
-    });
-  });
 
 
 
@@ -52,6 +28,8 @@ describe('', () => {
       expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
     });
   });
+
+
 describe('', () => {
     it('should return error', () => {
       const actualResult = MiddleCalculations(-2,2)
@@ -61,6 +39,8 @@ describe('', () => {
       expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
     });
   });
+
+
 describe('', () => {
     it('should return error', () => {
       const actualResult = MiddleCalculations(-2,-2)
@@ -70,6 +50,221 @@ describe('', () => {
       expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
     });
   });
+
+
+  describe('', () => {
+    it('should return error', () => {
+      const actualResult = MiddleCalculations(2)
+  
+      const expectedResult = "insufisiant input";
+  
+      expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+    });
+  });
+
+
+  describe('', () => {
+    it('should return error', () => {
+      const actualResult = MiddleCalculations(1,2,3)
+  
+      const expectedResult = "exesive input";
+  
+      expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+    });
+  });
+
+
+  describe('', () => {
+    it('should return error', () => {
+      const actualResult = MiddleCalculations(1,2,3,4,6,7,8)
+  
+      const expectedResult = "exesive input";
+  
+      expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+    });
+  });
+
+
+  describe('', () => {
+    it('should return error', () => {
+      const actualResult = MiddleCalculations({},{})
+  
+      const expectedResult = "input is not a numbner";
+  
+      expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+    });
+  });
+
+
+  describe('', () => {
+    it('should return error', () => {
+      const actualResult = MiddleCalculations([],[])
+  
+      const expectedResult = "input is not a numbner";
+  
+      expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+    });
+  });
+
+
+  describe('', () => {
+    it('should return error', () => {
+      const actualResult = MiddleCalculations("word","test")
+  
+      const expectedResult = "input is not a numbner";
+  
+      expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+    });
+  });
+
+
+  describe('', () => {
+    it('should return error', () => {
+      const actualResult = MiddleCalculations({},"test")
+  
+      const expectedResult = "input is not a numbner";
+  
+      expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+    });
+  });
+
+
+  describe('', () => {
+    it('should return error', () => {
+      const actualResult = MiddleCalculations([],"test")
+  
+      const expectedResult = "input is not a numbner";
+  
+      expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+    });
+  });
+
+  describe('', () => {
+    it('should return error', () => {
+      const actualResult = MiddleCalculations([],{})
+  
+      const expectedResult = "input is not a numbner";
+  
+      expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+    });
+  });
+
+
+
+// test calculations
+
+
+describe('', () => {
+  it('should return 2 и 2', () => {
+    const actualResult = MiddleCalculations(2,2)
+
+    const expectedResult = {
+      average:2,
+      geometricMean:2
+  };
+
+    expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+  });
+});
+
+
+describe('', () => {
+  it('should return 3.5 и 3.16228', () => {
+    const actualResult = MiddleCalculations(5,2)
+
+    const expectedResult = {
+      average:3.5,
+      geometricMean:3.16228
+  };
+
+    expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+  });
+});
+
+
+describe('', () => {
+  it('should return 1 и 0', () => {
+    const actualResult = MiddleCalculations(0,2)
+
+    const expectedResult = {
+      average:1,
+      geometricMean:0
+  };
+
+    expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+  });
+});
+
+
+describe('', () => {
+  it('should return 0 и 0', () => {
+    const actualResult = MiddleCalculations(0,0)
+
+    const expectedResult = {
+      average:0,
+      geometricMean:0
+  };
+
+    expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+  });
+});
+
+
+describe('', () => {
+  it('should return 0.02 и 0.01732', () => {
+    const actualResult = MiddleCalculations(0.01,0.03)
+
+    const expectedResult = {
+      average:0.02,
+      geometricMean:0.01732
+  };
+
+    expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+  });
+});
+
+
+describe('', () => {
+  it('should return 0.02 и 0.00173', () => {
+    const actualResult = MiddleCalculations(0.001,0.003)
+
+    const expectedResult = {
+      average:0.002,
+      geometricMean:0.00173
+  };
+
+    expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+  });
+});
+
+
+describe('', () => {
+  it('should return 5004999999.5 и 316227766.00103', () => {
+    const actualResult = MiddleCalculations(10000000,9999999999)
+
+    const expectedResult = {
+      average:5004999999.5,
+      geometricMean:316227766.00103
+  };
+
+    expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+  });
+});
+
+
+describe('', () => {
+  it('should return 5004999999.5 и 0', () => {
+    const actualResult = MiddleCalculations(0,9999999999)
+
+    const expectedResult = {
+      average:4999999999.5,
+      geometricMean:0
+  };
+
+    expect(actualResult).to.deep.equalInAnyOrder(expectedResult);
+  });
+});
+
 
 
 
